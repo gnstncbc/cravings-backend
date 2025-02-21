48c7f07f77d088fb0fe05cbe0951cb8f1088961e
Güneş Tan Cebeci <gnstncbc@gmail.com>
2025-02-21 20:09:19 +0300

    Initial commit

---
+++ src/main/java/com/gunes/cravings/CravingsApplication.java
@@ -0,0 +1,13 @@
+package com.gunes.cravings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CravingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CravingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunes/cravings/CravingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunes.cravings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CravingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
