7563383912a7f11860b284fcf06a784a80d72bed
gnstncbc <gnstncbc@gmail.com>
2025-04-08 19:54:03 +0300

    Remove unnecessary whitespace in CravingController

--- src/main/java/com/gunes/cravings/controller/CravingController.java
+++ src/main/java/com/gunes/cravings/controller/CravingController.java
@@ -16,6 +16,7 @@ public class CravingController {
     @Autowired
     private CravingRepository cravingRepository;
 
+    
     // Tüm kayıtları getir
     @GetMapping
     public List<Craving> getAllCravings() {
